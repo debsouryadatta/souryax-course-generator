c99713532483d67a2fb2a4377926644a3aee29a8
debsouryadatta <debsouryadatta@gmail.com>
2023-10-12 15:58:29 +0530

    update readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <Button className='text-red-400'>Hello World wow</Button>
+    <Button className='text-red-400'>Hello World 2</Button>
   )
 }
